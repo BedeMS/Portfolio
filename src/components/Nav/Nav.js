7df87fd00df5a435bcf63e23ba9a7db4180aab73
BedeMS <47581757+BedeMS@users.noreply.github.com>
2021-04-29 04:40:05 -0400

    Nav Component Ui Created. Animation still needed

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,12 +1,31 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "./Nav.css";
 
 function Nav(props) {
-    return (
-        <div className="Nav">
-            
-        </div>
-    )
+  return (
+    <div className="Nav">
+      <Link className="Nav__logo">Bede Marcos</Link>
+      <div className="Nav__list">
+        <Link className="Nav__list-item">About</Link>
+        <Link className="Nav__list-item">Projects</Link>
+        <Link className="Nav__list-item">Contact</Link>
+      </div>
+      <div className="Nav__social">
+        <Link className="Nav__social-icon github">Github: BedeMarcos</Link>
+        <Link className="Nav__social-icon">
+          <FontAwesomeIcon icon={["fab", "linkedin"]} />
+        </Link>
+        <Link className="Nav__social-icon">
+          <FontAwesomeIcon icon={["fab", "twitter"]} />
+        </Link>
+        <Link className="Nav__social-icon">
+          <FontAwesomeIcon icon={["fab", "github"]} />
+        </Link>
+      </div>
+    </div>
+  );
 }
 
 export default Nav;
--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,10 +1,16 @@
 import React from "react";
-import "./App";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { far } from "@fortawesome/free-regular-svg-icons";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import "./App.css";
 import Nav from "../../components/Nav/Nav";
+
+library.add(far, fab, fas);
 
 function App() {
   return (
-    <div className="App">
+    <div className="Home">
       <Nav />
     </div>
   );
