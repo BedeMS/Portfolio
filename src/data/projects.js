833c4719178d545749d3f4c971a60955b589aa62
BedeMS <47581757+BedeMS@users.noreply.github.com>
2021-05-03 03:50:11 -0400

    Created sample Data

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,7 +1,21 @@
+import uniqid from "uniqid";
+import jobIncBackground from "../assets/img/jobincBackground.png";
+import video from "../assets/videos/practice.mov";
+
 export default [
-    {
-        title: "Portfolio",
-        stack: ["react", "css"],
-        backgroundImg: '',
-    }
-]
+  {
+    id: uniqid(),
+    title: "Portfolio",
+    stack: ["react", "css"],
+    backgroundImg: jobIncBackground,
+    link: "www.bedemarcos.com",
+    github: "www.github.com/bedemarcos",
+    video: video,
+    features: [
+      {
+        desc: ["Cart Feature", "Updated on every page"],
+        featureImg: jobIncBackground,
+      },
+    ],
+  },
+];
