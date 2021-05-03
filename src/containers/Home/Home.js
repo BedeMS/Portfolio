90010bdd6a2977b699a289e59464a4b49d0ae3c7
BedeMS <47581757+BedeMS@users.noreply.github.com>
2021-05-03 11:56:11 -0400

    Home page -Added images

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,17 +1,34 @@
 import React from "react";
-import Nav from "../../components/Nav/Nav";
+import { Link } from "react-router-dom";
+import jobIncBackground from "../../data/jobInc/jobincBackground.png";
+import afroblockBackground from "../../data/afroBlock/afroblockBackground.png";
+import khariBackground from "../../data/khari/khariBackground.png";
+import myfinanceBackground from "../../data/myFinance/myfinanceBackground.png";
+import visitEthiopiaBackground from "../../data/visitEthiopia/visitEthiopiaBackground.png";
 import "./Home.css";
 
 function Home() {
   return (
     <div className="Home__main">
       <div className="Intro main"></div>
-      <div className="main Project"></div>
-      <div className="main Project"></div>
-      <div className="main Project"></div>
+      <div className="main Project">
+        <img src={jobIncBackground} alt="JobInc Background" />
+      </div>
+      <div className="main Project">
+        <img src={myfinanceBackground} alt="JobInc Background" />
+      </div>
+      <div className="main Project">
+        <img src={visitEthiopiaBackground} alt="JobInc Background" />
+      </div>
       <div className="Final main">
-        <div className="FinalProject"></div>
-        <div className="More"></div>
+        <div className="FinalProject">
+          <img src={khariBackground} alt="JobInc Background" />
+        </div>
+        <div className="More">
+          <Link className="More__link" to="/Projects">
+            More Projects
+          </Link>
+        </div>
       </div>
     </div>
   );
