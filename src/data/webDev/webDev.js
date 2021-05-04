aa3759615ded07885dbc58f591ef5e915454dd5d
BedeMS <47581757+BedeMS@users.noreply.github.com>
2021-05-04 13:15:19 -0400

    WebDev Data completed

--- src/data/webDev/webDev.js
+++ src/data/webDev/webDev.js
@@ -1,6 +1,11 @@
 import uniqid from "uniqid";
 import webdevBackground from "./webdevBackground.png";
-import video from "../../assets/videos/practice.mov";
+import responsive from "./responsive.png";
+import formData from "./formData.png";
+import form from "./form.png";
+import animatedBackground from "./animatedBackground.png";
+import animatedBackground2 from "./animatedBackground2.png";
+import video from "./webDevVideo.mov";
 
 export default {
   id: uniqid(),
@@ -14,9 +19,32 @@ export default {
   zoom: true,
   features: [
     {
-      desc: ["Cart Feature", "Updated on every page"],
-      featureImg: webdevBackground,
+      desc: ["Canvas Animation", "Lines Changing on Y and X", "Random Colors"],
+      featureImg: animatedBackground2,
+      // small: true,
+    },
+    {
+      desc: [
+        "Canvas Animation Change on Click",
+        "Lines connecting with Closest Dots",
+        "Random Colors",
+      ],
+      featureImg: animatedBackground,
+      reverse: true,
+    },
+    {
+      desc: ["User Data", "Form Validation"],
+      featureImg: form,
+    },
+    {
+      desc: ["Data Rendered Based on User Input", "User Specific Message"],
+      featureImg: formData,
       small: true,
+      reverse: true,
+    },
+    {
+      desc: ["Responsive Design"],
+      featureImg: responsive,
     },
   ],
 };
