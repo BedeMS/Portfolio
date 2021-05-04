c3ddf9164568085a99db0e3b1de9ccd63b1fc75c
BedeMS <47581757+BedeMS@users.noreply.github.com>
2021-05-04 16:20:25 -0400

    Home page changed entirely

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -11,27 +11,30 @@ function Home() {
   return (
     <div className="Home__main">
       <div className="Intro main"></div>
-      <div className="main Project">
-        <img src={jobIncBackground} alt="JobInc Background" />
-      </div>
-      <div className="main Project">
-        <img src={myfinanceBackground} alt="JobInc Background" />
-      </div>
-      <div className="main Project">
-        <img src={visitEthiopiaBackground} alt="JobInc Background" />
-      </div>
-      <div className="Final main">
-        <div className="FinalProject">
-          <img src={khariBackground} alt="JobInc Background" />
-        </div>
-        <div className="More__container">
-          <Link className="More__link" to="/Projects">
-            More Projects
-          </Link>
-        </div>
-      </div>
     </div>
   );
 }
 
 export default Home;
+
+
+
+{/* <div className="main Project">
+<img src={jobIncBackground} alt="JobInc Background" />
+</div>
+<div className="main Project">
+<img src={myfinanceBackground} alt="JobInc Background" />
+</div>
+<div className="main Project">
+<img src={visitEthiopiaBackground} alt="JobInc Background" />
+</div>
+<div className="Final main">
+<div className="FinalProject">
+  <img src={khariBackground} alt="JobInc Background" />
+</div>
+<div className="More__container">
+  <Link className="More__link" to="/Projects">
+    More Projects
+  </Link>
+</div>
+</div> */}
