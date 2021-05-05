e3887b4d6e3b80900785453178b6837306f52965
BedeMS <47581757+BedeMS@users.noreply.github.com>
2021-05-05 16:47:09 -0400

    Home page fully updated

--- src/containers/Contact/Confirmation/Confirmation.js
+++ src/containers/Contact/Confirmation/Confirmation.js
@@ -8,7 +8,7 @@ function Confirmation(props) {
         <h1 className="Confirmation__title">Thank You for your message.</h1>
         <p className="Confirmation__desc">
           I've recieved your email and will respond as soon as possible. I
-          appreciate you taking the time to look through my portfolio.
+          appreciate you for taking the time to look through my portfolio.
         </p>
       </div>
     </div>
--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,25 +1,45 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import jobIncBackground from "../../data/jobInc/jobincBackground.png";
-import afroblockBackground from "../../data/afroBlock/afroblockBackground.png";
-import khariBackground from "../../data/khari/khariBackground.png";
-import myfinanceBackground from "../../data/myFinance/myfinanceBackground.png";
-import visitEthiopiaBackground from "../../data/visitEthiopia/visitEthiopiaBackground.png";
 import "./Home.css";
 
-function Home() {
+function Home(props) {
   return (
     <div className="Home__main">
-      <div className="Intro main"></div>
+      <div className="Intro">
+        <h1 className="Intro__b">B</h1>
+        <div className="perspective">
+          <div className="Project__a">
+            <div className="project__in"></div>
+            <h1 className="Project__title">Job Inc.</h1>
+          </div>
+          <div className="Project__b">
+            <div className="project__in"></div>
+            <h1 className="Project__title">Visit Ethiopia</h1>
+
+          </div>
+        </div>
+      </div>
+      <div className="Intro">
+        <h1 className="Intro__m">M</h1>
+        <div className="perspective">
+          <div className="Project__c">
+            <div className="project__in"></div>
+            <h1 className="Project__title">Web Dev Roadmap</h1>
+          </div>
+          <div className="Project__d">
+            <div className="project__in"></div>
+            <Link to="/Projects" className="Project__More">More Projects</Link>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
 
 export default Home;
 
-
-
-{/* <div className="main Project">
+{
+  /* <div className="main Project">
 <img src={jobIncBackground} alt="JobInc Background" />
 </div>
 <div className="main Project">
@@ -37,4 +57,5 @@ export default Home;
     More Projects
   </Link>
 </div>
-</div> */}
+</div> */
+}
