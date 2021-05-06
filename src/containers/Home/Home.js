cdc38d01c2b18c308f2d582186cf1365c85da683
BedeMS <47581757+BedeMS@users.noreply.github.com>
2021-05-06 06:34:33 -0400

    CSS MediaQuery clean up

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -56,24 +56,4 @@ function Home({history, openModal }) {
 
 export default Home;
 
-{
-  /* <div className="main Project">
-<img src={jobIncBackground} alt="JobInc Background" />
-</div>
-<div className="main Project">
-<img src={myfinanceBackground} alt="JobInc Background" />
-</div>
-<div className="main Project">
-<img src={visitEthiopiaBackground} alt="JobInc Background" />
-</div>
-<div className="Final main">
-<div className="FinalProject">
-  <img src={khariBackground} alt="JobInc Background" />
-</div>
-<div className="More__container">
-  <Link className="More__link" to="/Projects">
-    More Projects
-  </Link>
-</div>
-</div> */
-}
+
